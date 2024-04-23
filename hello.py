212e9c64442877b3f4680dba8ba553b79f2e6204
vytro <brayan.bbgc@gmail.com>
2024-04-23 14:29:20 -0400

    agregacion de variables

--- hello.py
+++ hello.py
@@ -4,6 +4,9 @@ def hello():
     #print("Hello, World!")
     message = os.environ.get("A_VARIABLE", "World")
     print(f"Hello, {message}!")
+    print(f"Se puede ver el secreto: {os.environ.get('A_VARIABLE', 'no hay')}")
+    message2 = os.environ.get("B_VARIABLE", "World")
+    print(f"Hello, {message2}!")
 
 if __name__ == "__main__":
     hello()
